b22fe6cc9c35f0a3d9b12376f017647d8c85a799
Laila_Agustina <lailaagustina025@gmail.com>
2018-01-19 23:02:45 -0800

    membuat class

---
+++ kameraa.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class kamera {
+    public:
+        void jeniskamera(string kamera);
+        string merkkamera;
+        void tipekamera(string tipe);
+        void ukurankamera(string ukuran);
+        void hargakamera (int harga);
+    
+                
+        };
